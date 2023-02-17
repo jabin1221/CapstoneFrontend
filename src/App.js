c57d34d161a94ff52ae7ba119f9240d79b7b84e9
tjgus-A <audreyahn@naver.com>
2023-02-17 20:24:11 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 import AuthCreatePage from "./Routes/AuthCreatePage";
 import LoginPage from "./Routes/LoginPage";
@@ -6,6 +7,11 @@ import MainPage from "./Routes/MainPage";
 import MyPage from "./Routes/MyPage";
 import BuyPage from "./Routes/BuyPage";
 import SellPage from "./Routes/SellPage";
+import DetailPage from "./Routes/DetailPage";
+import Products from "./Routes/data";
+import Layout from './Components/Layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import {
     BrowserRouter,
     HashRouter as Router,
@@ -13,9 +19,15 @@ import {
     Routes
   } from "react-router-dom";
 
+
 function App() {
+  
+  let [Product] = useState(Products);
   return (
+   
+    
   <BrowserRouter>
+     <Layout></Layout>
     <Routes>
       <Route exact path="/" element={<LoginPage />}></Route>
       <Route exact path="/SignUp" element={<AuthCreatePage />}></Route>
@@ -23,6 +35,7 @@ function App() {
       <Route exact path="/MyPage" element={<MyPage />}></Route>
       <Route exact path="/SellPage" element={<SellPage />}></Route>
       <Route exact path="/BuyPage" element={<BuyPage />}></Route>
+      <Route path="/DetailPage/:id" element={<DetailPage Product ={ Product }/>} />
     </Routes>
     </BrowserRouter>
 
--- src/Routes/MainPage.js
+++ src/Routes/MainPage.js
@@ -1,13 +1,21 @@
-import React, { useEffect, useState} from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import Products from "./data";
+import Item from "./Products.js";
 
-function MainPage() {
+function MainPage(props) {
+  let [Product] = useState(Products);
+  return (
+    <div>
+      <div className="col-md-5">
+        <h3>MainPage</h3>
 
-
-
-return(
-    <h3>MainPage</h3>
-)
+        {Product.map(function (product, i) {
+          return <Item product={Product[i]} i={i} key={i} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
